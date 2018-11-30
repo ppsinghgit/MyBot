7fc8a820c11914298b5bb3393f727024616a93b3
Vidush <ssb.trainee@idsil.com>
2018-11-30 18:06:04 +0530

    add intent to the bot

---
+++ BotDemo/Dialogs/Greeting/GreetingState.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotBuilderSamples
+{
+    /// <summary>
+    /// User state properties for Greeting.
+    /// </summary>
+    public class GreetingState
+    {
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Job { get; set; }
+    }
+}
